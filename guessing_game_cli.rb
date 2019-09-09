d4b272d702807fcb4c9dc165884d8b33f44009c9
Adeja Lendor <alendor6@gmail.com>
2019-09-09 19:45:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,10 +4,4 @@ def run_guessing_game
   input = gets.chomp
   if input == n
     puts "You guessed the correct number!"
-  else puts "Sorry! The computer guessed ."
-    input = gets.chomp
-    if input = "exit"
-      puts "Goodbye!"
-    end
-  end
 end
