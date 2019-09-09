aac88d14cb155a4ee8e1ae9124fa9f8bc9fa91b0
Adeja Lendor <alendor6@gmail.com>
2019-09-09 20:19:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   input = gets.chomp
   if input = "exit"
     p "Goodbye!"
-  elsif input == random_number
+  elsif input == random_number.to_s 
   p "You guessed the correct number!"
 else
   
