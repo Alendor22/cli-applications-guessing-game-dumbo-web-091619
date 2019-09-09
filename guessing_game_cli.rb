858fbfe004536288881eafc9be41c55230b1ca93
Adeja Lendor <alendor6@gmail.com>
2019-09-09 19:48:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   n = rand(6) + 1 
-  puts "guess a number between 1 and 6."
+  p"guess a number between 1 and 6."
   input = gets.chomp
   if input == n
     p "You guessed the correct number!"
